038c7e8ea22ea6927078178edf1bf3c9193587f3
Aryna <Shepel1992@mail.ru>
2019-02-16 23:59:07 +0200

    update

--- build/js/index.js
+++ build/js/index.js
@@ -115,7 +115,7 @@ function appStart(login) {
 }
 
 const arrayOfQuestions = [{
-  name: "Oma",
+  name: "Syvälliset",
   color: "rgb(255, 0, 106)"
 }, {
   name: "Klassikot",
@@ -166,7 +166,140 @@ function showFriends() {
 ;
 
 function showQuestions() {
-  let htmlCode = _AryJs_js__WEBPACK_IMPORTED_MODULE_1__["default"].arrayToHtml(arrayOfQuestions.map(element => `<li class="single_question_type" style="--my-color-var: ${element.color};"><p>${element.name}</p><svg xmlns="http://www.w3.org/2000/svg" width="35" height="35" viewBox="0 0 24 24"><path d="M8.59 16.59L13.17 12 8.59 7.41 10 6l6 6-6 6-1.41-1.41z"/><path fill="none" d="M0 0h24v24H0V0z"/></svg></li>`));
+  let htmlCode = `<div class="questions">
+
+    <ul class="questions-list">
+  
+      <li class="question">
+        <div class="question-main-section">
+             <h2 class="question-title">
+               Syvälliset
+               <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M7.41 8.59L12 13.17l4.59-4.58L18 10l-6 6-6-6 1.41-1.41z"/><path fill="none" d="M0 0h24v24H0V0z"/></svg>
+             </h2>
+         </div>
+        <ul class="typed-questions">
+              <li class="t-question">
+                Minkälaisena persoonana koet ihmisten näkevän sinut? <br>
+                <input class="answer">
+              </li>
+              <li class="t-question">
+                Jos joku antaisi sinulle kirjan ja alkaisit lukemaan sitä, mutta huomaisit että kirja<br>
+                <input class="answer">
+              </li>
+              <li class="t-question">
+                Kertoo elämästäsi, niin lukisitko sen loppuun? <br>
+                <input class="answer">
+              </li>
+              <li class="t-question">
+                Mikä on mielestäsi hyvää piristystä parisuhteelle?<br>
+                <input class="answer">
+              </li>
+              <li class="t-question">
+                Kolme hyvää puolta sinussa?<br>
+                <input class="answer">
+              </li>
+              <li class="t-question">
+                Kolme huonoa puolta sinussa?<br>
+                <input class="answer">
+              </li>
+              <li class="t-question">
+                <input class="oma-question" placeholder="Oma kysymys"></input><br>
+                <input class="answer">
+              </li>
+              <button class="next">Lähettää</button>
+        </ul>
+      </li>
+  
+  
+  
+  
+      <li class="question">
+        <div class="question-main-section">
+            <h2 class="question-title">
+              Random
+              <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M7.41 8.59L12 13.17l4.59-4.58L18 10l-6 6-6-6 1.41-1.41z"/><path fill="none" d="M0 0h24v24H0V0z"/></svg>
+            </h2>
+        </div>
+        <ul class="typed-questions">
+              <li class="t-question">
+                Viimeisin ostoksesi? <br>
+                <input class="answer">
+              </li>
+              <li class="t-question">
+                Viimeksi lukemasi kirja?<br>
+                <input class="answer">
+              </li>
+              <li class="t-question">
+                Kamalin tatuointi-idea? <br>
+                <input class="answer">
+              </li>
+              <li class="t-question">
+                Mitä keräilet/ olet keräillyt?<br>
+                <input class="answer">
+              </li>
+              <li class="t-question">
+                Mikä on pahin pelkosi?<br>
+                <input class="answer">
+              </li>
+              <li class="t-question">
+                Mitä pidät taskussasi?<br>
+                <input class="answer">
+              </li>
+              <li class="t-question">
+                <input class="oma-question" placeholder="Oma kysymys"></input><br>
+                <input class="answer">
+              </li>
+              <button class="next">Lähettää</button>
+  
+        </ul>
+      </li>
+  
+      <li class="question">
+        <div class="question-main-section">
+            <h2 class="question-title">
+              Kumpi
+              <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M7.41 8.59L12 13.17l4.59-4.58L18 10l-6 6-6-6 1.41-1.41z"/><path fill="none" d="M0 0h24v24H0V0z"/></svg>
+            </h2>
+        </div>
+        <ul class="typed-questions">
+              <li class="t-question">
+              Parisuhde vai sinkkuus? <br>
+                <input class="answer">
+              </li>
+              <li class="t-question">
+                Pieni vai iso laukku?<br>
+                <input class="answer">
+              </li>
+              <li class="t-question">
+              Suorat vai kiharat hiukset? <br>
+                <input class="answer">
+              </li>
+              <li class="t-question">
+                Timantteja vai helmiä?<br>
+                <input class="answer">
+              </li>
+              <li class="t-question">
+                Manikyyri vai pedikyyri?<br>
+                <input class="answer">
+              </li>
+              <li class="t-question">
+              Pastellivärit vai neonvärit?<br>
+                <input class="answer">
+              </li>
+              <li class="t-question">
+                <input class="oma-question" placeholder="Oma kysymys"></input><br>
+                <input class="answer">
+              </li>
+              <button class="next">Lähettää</button>
+  
+        </ul>
+      </li>
+  
+  
+    </ul>
+  </div>`; //let htmlCode=Ar.arrayToHtml(arrayOfQuestions.map((element)=>    `<li class="single_question_type" style="--my-color-var: ${element.color};"><p>${element.name}</p><svg class="svg" xmlns="http://www.w3.org/2000/svg" width="35" height="35" viewBox="0 0 24 24"><path d="M8.59 16.59L13.17 12 8.59 7.41 10 6l6 6-6 6-1.41-1.41z"/><path fill="none" d="M0 0h24v24H0V0z"/></svg>
+  // arrayOfQuestions[0].ul=syQul;</li>`));
+
   _AryJs_js__WEBPACK_IMPORTED_MODULE_1__["default"].showhtml("ul_root", htmlCode);
 }
 
--- dev/js/index.js
+++ dev/js/index.js
@@ -10,6 +10,10 @@ var button = document.querySelector('.kaverit').addEventListener('click', showFr
 var button = document.querySelector('.viestit').addEventListener('click', showMessages);
 var button = document.querySelector('.kysy').addEventListener('click', showQuestions);
 
+
+
+
+
 let user_name ="";
 login.start(appStart);
 
@@ -22,13 +26,14 @@ function appStart(login){
 }
 
 const arrayOfQuestions=[
-    {name: "Oma", color: "rgb(255, 0, 106)"},
-    {name: "Klassikot", color: "rgb(255, 110, 106)"},
-    {name: "Lempi", color: "rgb(255, 110, 106)"},
-    {name: "Kumpi", color: "rgb(255, 220, 106)"},
-    {name: "new type", color: "rgb(255, 220, 106)"},
-    {name: "Kerro jotain siita kun...", color: "rgb(255, 20, 106)"}
+    {name: "Syvälliset", color: "rgb(255, 0, 106)", },
+    {name: "Klassikot", color: "rgb(255, 110, 106)", },
+    {name: "Lempi", color: "rgb(255, 110, 106)", },
+    {name: "Kumpi", color: "rgb(255, 220, 106)",},
+    {name: "new type", color: "rgb(255, 220, 106)", },
+    {name: "Kerro jotain siita kun...", color: "rgb(255, 20, 106)",}
 ];
+
 const arrayOfFriends=[
     {id:"1", name: "Aryna", icon: 'pictures/profile.png'},
     {id:"2", name: "Gleb", icon: 'pictures/profile1.png'},
@@ -56,9 +61,140 @@ function showFriends(){
     });
 };
 function showQuestions(){
-    let htmlCode=Ar.arrayToHtml(arrayOfQuestions.map((element)=>
-    `<li class="single_question_type" style="--my-color-var: ${element.color};"><p>${element.name}</p><svg xmlns="http://www.w3.org/2000/svg" width="35" height="35" viewBox="0 0 24 24"><path d="M8.59 16.59L13.17 12 8.59 7.41 10 6l6 6-6 6-1.41-1.41z"/><path fill="none" d="M0 0h24v24H0V0z"/></svg></li>`
-    ));
+    let htmlCode=`<div class="questions">
+
+    <ul class="questions-list">
+  
+      <li class="question">
+        <div class="question-main-section">
+             <h2 class="question-title">
+               Syvälliset
+               <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M7.41 8.59L12 13.17l4.59-4.58L18 10l-6 6-6-6 1.41-1.41z"/><path fill="none" d="M0 0h24v24H0V0z"/></svg>
+             </h2>
+         </div>
+        <ul class="typed-questions">
+              <li class="t-question">
+                Minkälaisena persoonana koet ihmisten näkevän sinut? <br>
+                <input class="answer">
+              </li>
+              <li class="t-question">
+                Jos joku antaisi sinulle kirjan ja alkaisit lukemaan sitä, mutta huomaisit että kirja<br>
+                <input class="answer">
+              </li>
+              <li class="t-question">
+                Kertoo elämästäsi, niin lukisitko sen loppuun? <br>
+                <input class="answer">
+              </li>
+              <li class="t-question">
+                Mikä on mielestäsi hyvää piristystä parisuhteelle?<br>
+                <input class="answer">
+              </li>
+              <li class="t-question">
+                Kolme hyvää puolta sinussa?<br>
+                <input class="answer">
+              </li>
+              <li class="t-question">
+                Kolme huonoa puolta sinussa?<br>
+                <input class="answer">
+              </li>
+              <li class="t-question">
+                <input class="oma-question" placeholder="Oma kysymys"></input><br>
+                <input class="answer">
+              </li>
+              <button class="next">Lähettää</button>
+        </ul>
+      </li>
+  
+  
+  
+  
+      <li class="question">
+        <div class="question-main-section">
+            <h2 class="question-title">
+              Random
+              <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M7.41 8.59L12 13.17l4.59-4.58L18 10l-6 6-6-6 1.41-1.41z"/><path fill="none" d="M0 0h24v24H0V0z"/></svg>
+            </h2>
+        </div>
+        <ul class="typed-questions">
+              <li class="t-question">
+                Viimeisin ostoksesi? <br>
+                <input class="answer">
+              </li>
+              <li class="t-question">
+                Viimeksi lukemasi kirja?<br>
+                <input class="answer">
+              </li>
+              <li class="t-question">
+                Kamalin tatuointi-idea? <br>
+                <input class="answer">
+              </li>
+              <li class="t-question">
+                Mitä keräilet/ olet keräillyt?<br>
+                <input class="answer">
+              </li>
+              <li class="t-question">
+                Mikä on pahin pelkosi?<br>
+                <input class="answer">
+              </li>
+              <li class="t-question">
+                Mitä pidät taskussasi?<br>
+                <input class="answer">
+              </li>
+              <li class="t-question">
+                <input class="oma-question" placeholder="Oma kysymys"></input><br>
+                <input class="answer">
+              </li>
+              <button class="next">Lähettää</button>
+  
+        </ul>
+      </li>
+  
+      <li class="question">
+        <div class="question-main-section">
+            <h2 class="question-title">
+              Kumpi
+              <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M7.41 8.59L12 13.17l4.59-4.58L18 10l-6 6-6-6 1.41-1.41z"/><path fill="none" d="M0 0h24v24H0V0z"/></svg>
+            </h2>
+        </div>
+        <ul class="typed-questions">
+              <li class="t-question">
+              Parisuhde vai sinkkuus? <br>
+                <input class="answer">
+              </li>
+              <li class="t-question">
+                Pieni vai iso laukku?<br>
+                <input class="answer">
+              </li>
+              <li class="t-question">
+              Suorat vai kiharat hiukset? <br>
+                <input class="answer">
+              </li>
+              <li class="t-question">
+                Timantteja vai helmiä?<br>
+                <input class="answer">
+              </li>
+              <li class="t-question">
+                Manikyyri vai pedikyyri?<br>
+                <input class="answer">
+              </li>
+              <li class="t-question">
+              Pastellivärit vai neonvärit?<br>
+                <input class="answer">
+              </li>
+              <li class="t-question">
+                <input class="oma-question" placeholder="Oma kysymys"></input><br>
+                <input class="answer">
+              </li>
+              <button class="next">Lähettää</button>
+  
+        </ul>
+      </li>
+  
+  
+    </ul>
+  </div>`;
+    //let htmlCode=Ar.arrayToHtml(arrayOfQuestions.map((element)=>    `<li class="single_question_type" style="--my-color-var: ${element.color};"><p>${element.name}</p><svg class="svg" xmlns="http://www.w3.org/2000/svg" width="35" height="35" viewBox="0 0 24 24"><path d="M8.59 16.59L13.17 12 8.59 7.41 10 6l6 6-6 6-1.41-1.41z"/><path fill="none" d="M0 0h24v24H0V0z"/></svg>
+   // arrayOfQuestions[0].ul=syQul;</li>`));
     Ar.showhtml("ul_root", htmlCode);
 };
 function showMessages(){
